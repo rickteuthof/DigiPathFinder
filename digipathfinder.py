6bb13648595bc1dc848ee2597a1d6c5959baecd2
Rick Teuthof <rmt22198@outlook.com>
2021-07-12 17:58:33 +0200

    Fixed with weights

--- digipathfinder.py
+++ digipathfinder.py
@@ -10,21 +10,23 @@ with open('database.json') as json_file:
 
 for digimon in database:
     for prev_digimon in database[digimon]['prev']:
-        G.add_edge(digimon, prev_digimon, direction='down')
+        G.add_edge(digimon, prev_digimon, direction='down', weight=0)
 
     for next_digimon in database[digimon]['next']:
-        G.add_edge(digimon, next_digimon, direction='up')
+        weight = int(database[digimon]['next'][next_digimon]['Level'])
+        G.add_edge(digimon, next_digimon, direction='up', weight=weight)
 
 with open('skill_database.json') as json_file:
     skill_database = json.load(json_file)
 
-def get_path_with_skills(start, end, withskills=[]):
+
+def get_path(start, end, withskills=[]):
     nodes = [start]
     for skill in withskills:
         nodes.append(skill_database[skill][0][0])
     nodes.append(end)
     for start, end, skill in zip(nodes[:-1], nodes[1:], withskills+['']):
-        path = nx.shortest_path(G, start, end)
+        path = nx.dijkstra_path(G, start, end)
         for i in range(len(path) - 1):
             direction = G.get_edge_data(path[i], path[i+1])[0]['direction']
             print("%s -> %s -> %s" % (path[i], direction, path[i+1]))
@@ -32,8 +34,7 @@ def get_path_with_skills(start, end, withskills=[]):
             print("Get skill %s" % skill)
 
 
-get_path_with_skills('Clockmon', 'Angewomon', [
-                     'Wolkenapalm I', 'Nanomachine Break I', 'Grand Rock III', "Final Aura"])
+get_path('LadyDevimon', 'Angewomon', withskills=["Gaia Element I"])
 # pos = nx.spring_layout(G)
 # nx.draw(G, pos)
 # labels = nx.draw_networkx_labels(G, pos)
